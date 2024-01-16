243666c0c2103a06a95c196e6b6121856f983c5d
Yashvipatel295 <117625936+Yashvipatel295@users.noreply.github.com>
2024-01-16 12:59:42 +0530

    Add files via upload
    16/01/2024

---
+++ index.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+    integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <a class="nav-link" href="#">Features</a>
+              <a class="nav-link" href="#">Pricing</a>
+              <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <p>
+        <span>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis, molestias! Hic doloremque consectetur, fuga dignissimos aliquam nesciunt veritatis qui in impedit excepturi corrupti dicta, necessitatibus suscipit voluptatem, ducimus aliquid optio.</p>
+            <p>Quis temporibus illum rem neque cumque. Eveniet sed officia reiciendis necessitatibus ipsam, iste est facere illo ullam ex id porro nam quidem rem mollitia molestiae dolores debitis voluptas. Illo, expedita.</p>
+            <p>Dolore eveniet similique quas nam consectetur voluptatum minus voluptatem enim, quaerat quia aperiam, asperiores cupiditate provident corrupti odit eaque architecto quod dignissimos, modi repellendus amet unde expedita neque cumque. Et.</p>
+        </span>
+      </p>
+      <p><span>copyright &copy; <a href="#">my blog</a></span></p>
+    </div>
+
+
+
+</body>
+</html>
